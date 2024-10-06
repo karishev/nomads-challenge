51ca974976637effe56f2679058a20f3312a269c
karishev <sk8795@nyu.edu>
2024-10-06 23:53:40 +0400

    feat: show graphics plot

--- src/tools/countryInfo.js
+++ src/tools/countryInfo.js
@@ -4,38 +4,45 @@ export const Info = ({ data, setClose }) => {
   // console.log(props);
 
   return (
-    <div className={styles.country_info}>
-      <div onClick={setClose} className={styles.close}>
-        <img src="/close_button.svg" alt="close" />
-      </div>
-      <h2>{data.name}</h2>
-      <div>
-        <ul className={styles.recordings}>
-          {data.affected_areas ? (
-            <li>Affected Areas: {data.affected_areas.join(", ")}</li>
+    <>
+      <div className={styles.country_info}>
+        <div onClick={setClose} className={styles.close}>
+          <img src="/close_button.svg" alt="close" />
+        </div>
+        <h2>{data.name}</h2>
+        <div>
+          <ul className={styles.recordings}>
+            {data.affected_areas ? (
+              <li>Affected Areas: {data.affected_areas.join(", ")}</li>
+            ) : (
+              ""
+            )}
+            {data.death_toll ? <li>Death Toll: {data.death_toll}</li> : ""}
+            {data.missing ? <li>Missing: {data.missing}</li> : ""}
+            {data.affected_population ? (
+              <li>Affected Population: {data.affected_population}</li>
+            ) : (
+              ""
+            )}
+          </ul>
+          {data.effects ? <p>{data.effects}</p> : ""}
+          {data.impact ? (
+            <p dangerouslySetInnerHTML={{ __html: data.impact }} />
           ) : (
             ""
           )}
-          {data.death_toll ? <li>Death Toll: {data.death_toll}</li> : ""}
-          {data.missing ? <li>Missing: {data.missing}</li> : ""}
-          {data.affected_population ? (
-            <li>Affected Population: {data.affected_population}</li>
+          {data.gender_data ? (
+            <p dangerouslySetInnerHTML={{ __html: data.gender_data }} />
           ) : (
             ""
           )}
-        </ul>
-        {data.effects ? <p>{data.effects}</p> : ""}
-        {data.impact ? (
-          <p dangerouslySetInnerHTML={{ __html: data.impact }} />
-        ) : (
-          ""
-        )}
-        {data.gender_data ? (
-          <p dangerouslySetInnerHTML={{ __html: data.gender_data }} />
-        ) : (
-          ""
-        )}
+        </div>
       </div>
-    </div>
+      <div className={styles.plots}>
+        <div className={styles.plot}>
+          <img src={`disasters/${data.plot}`} alt="plot" />
+        </div>
+      </div>
+    </>
   );
 };
